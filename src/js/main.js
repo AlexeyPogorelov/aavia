6abbf68b956b73c14260042a8247e0f9a2ecab7d
AlexeyPogorelov <hirurg555@gmail.com>
2016-01-06 03:38:17 +0200

    added pixelperfect

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+$('#main-overlay').on('click', function () {
+    $('.main-overlay').toggleClass('hidden');
+});
+$('#grid-overlay').on('click', function () {
+    $('.grid-overlay').toggleClass('hidden');
+});
