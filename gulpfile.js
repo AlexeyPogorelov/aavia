b61dc143195b60e6ca437f5e372594d81aa47d75
AlexeyPogorelov <hirurg555@gmail.com>
2016-02-03 22:22:56 +0200

    gulp server

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+	browserSync = require('browser-sync'),
+	reload = require('browser-sync').reload;
+
+var serverConfig = {
+	server: {
+		baseDir: "./src"
+	},
+	// tunnel: true,
+	host: 'localhost',
+	port: 63341,
+	logPrefix: "aavia"
+};
+
+// SERVER
+gulp.task('browser-sync', function() {
+	browserSync.init(serverConfig);
+});
+
+// RELOAD
+gulp.task('browser-reload', function() {
+	// console.log('test');
+	reload();
+	// gulp.dest('dist/css')
+	// 	.pipe(reload({stream:true}));
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
+
+// WATCH
+gulp.task('watch', function(){
+	gulp.watch('src/**/*.*', ['browser-reload']);
+	// gulp.watch('**/*.*', reload());
+});
